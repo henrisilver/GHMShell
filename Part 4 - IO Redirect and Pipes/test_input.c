9143cd908bd72c56e68ddc3eb1a894e1e59005db
giulianoprado <giulianoprado@gmail.com>
2016-06-21 21:54:27 -0300

    coisas extras de teste

---
+++ Part 4 - IO Redirect and Pipes/test_input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	char texto[20];
+	scanf("%s",texto);
+	printf("OI! RECEBI <%s>!", texto);
+	return 0;
+}
---
+++ Part 4 - IO Redirect and Pipes/test_sum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	int valor;
+	scanf("%d",&valor);
+	printf("%d <<<< SOMA\n", valor+1);
+	return 0;
+}
